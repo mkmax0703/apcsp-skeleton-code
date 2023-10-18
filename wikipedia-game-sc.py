5973c9c828067331dff020418f40978e8c0ae591
Maximus Kanounji <mkmax0703@gmail.com>
2023-10-18 21:09:50 +0000

    Added bfs thingy

--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -2,7 +2,7 @@ from queue import Queue
 import wikipediaapi
 import time
 
-user_agent = "MsOrret'sWikipediaGame/1.0 (orret.deborah@pusd.us)"
+user_agent = "Maximus'sWikipediaGame/1.0 (ka7661a0703@gmail.com)"
 
 wiki_wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
@@ -21,7 +21,39 @@ def wikipedia_game_solver(start_page, target_page):
     start_time = time.time()
   
     # FINISH THE CODE HERE
+    visited = []
+    queue = Queue()
+    path = []
+    parent = {}
 
+    queue.put(start_page.title)
+    visited.append(start_page.title)
+
+    while not queue.empty():
+    
+        current_title = queue.get()
+
+        if current_title == target_page.title:
+            break
+
+        #visit my current_title
+        visited.append(current_title)
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
+
+        for node in next_level:
+            if node not in visited:
+                queue.put(node)
+                parent[node] = current_title
+
+    child = target_page.title
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append (start_page.title)
+    path.reverse()
+        
+        
     end_time = time.time()
     print("This algorithm took", end_time-start_time, "seconds to run!")
   
